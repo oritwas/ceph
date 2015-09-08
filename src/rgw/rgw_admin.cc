3fccc26a3f4bfd09e57cd1bc0d43e9b01979a9d7
Casey Bodley <cbodley@redhat.com>
2015-09-08 13:47:48 -0400

    radosgw-admin: add json output to create commands
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -1856,6 +1856,9 @@ int main(int argc, char **argv)
 	  cerr << "ERROR: couldn't create realm " << realm_name << ": " << cpp_strerror(-ret) << std::endl;
 	  return ret;
 	}
+	encode_json("realm", realm, formatter);
+	formatter->flush(cout);
+	cout << std::endl;
       }
       break;
     case OPT_REALM_DELETE:
@@ -2012,6 +2015,10 @@ int main(int argc, char **argv)
 	  cerr << "failed to create zonegroup" << zonegroup_name << ": " << cpp_strerror(-ret) << std::endl;
 	  return -ret;
 	}
+
+	encode_json("zonegroup", zonegroup, formatter);
+	formatter->flush(cout);
+	cout << std::endl;
       }
       break;
     case OPT_ZONEGROUP_DEFAULT:
@@ -2240,6 +2247,10 @@ int main(int argc, char **argv)
 	       << cpp_strerror(-ret) << std::endl;
 	  return ret;
 	}
+
+	encode_json("zone", zone, formatter);
+	formatter->flush(cout);
+	cout << std::endl;
       }
       break;
    case OPT_ZONE_DELETE:
